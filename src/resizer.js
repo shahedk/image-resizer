cc1048f969ad3d35e34265ceaa29708b8d8fc94d
Lionel Sengkouvanh <lionel.sengkouvanh@data-home.fr>
2023-01-25 10:03:21 +0100

    rotate images to display nicely before resize, otherwise orientation information is lost

--- src/resizer.js
+++ src/resizer.js
@@ -9,16 +9,33 @@ async function resize(filePath, params) {
         const resizedImageName = paramHelper.getResizedFileName(params);
         const resizedImagePath = './tmp/' + resizedImageName;
 
-            await sharp(filePath)
+        const original = await sharp(filePath)
+        const metadata = await original.metadata()
+        let targetImage = original
                 .resize(params.width, params.height, {
                     kernel: sharp.kernel.nearest,
                     fit: params.fit,
                     background: config.ImageBackground
                 })
                 .toFormat(params.format)
-                .toFile(resizedImagePath);
+       
+        if(Number.isInteger(metadata.orientation)){
+            switch (metadata.orientation) {
+                case 3:
+                    targetImage = targetImage.rotate(180)
+                    break;
+                case 6:
+                    targetImage = targetImage.rotate(90)
+                    break;
+                case 8:
+                    targetImage = targetImage.rotate(-90)
+                    break;
+                default:
+                    break
+            }
+        }
 
-
+        await targetImage.toFile(resizedImagePath)
         return Promise.resolve(resizedImageName);
     }
     catch (e) {
